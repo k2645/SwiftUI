6c0c956fd57b18de661ea3318b6bea0e34488312
k2645 <k2645@naver.com>
2023-06-30 18:16:17 +0900

    [feat] project 생성

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by 김영현 on 2023/06/30.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
