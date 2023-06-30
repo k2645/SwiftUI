1471c905a19c70259d8a59b17b463a8fee83522a
k2645 <k2645@naver.com>
2023-07-01 08:53:12 +0900

    [feat] asset에 image 추가, data model 생성하기, file 그룹화

---
+++ Landmarks/Landmarks/Model/Landmark.swift
@@ -0,0 +1,32 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by 김영현 on 2023/07/01.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+    
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+    
+    private var coordinates: Coordinates
+
+    struct Coordinates: Hashable, Codable {
+        var latitude: Double
+        var longitude: Double
+    }
+}
+
+
