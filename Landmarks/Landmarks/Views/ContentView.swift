efea8fb37913809c359fd9f3a92391977a6c68a4
k2645 <k2645@naver.com>
2023-07-01 09:20:14 +0900

    [feat] navigationView 추가, navigation link 추가

--- Landmarks/Landmarks/Views/ContentView.swift
+++ Landmarks/Landmarks/Views/ContentView.swift
@@ -9,39 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            
-            VStack(alignment: .leading) {
-                Text("Hallstatter sea")
-                    .font(.title)
-                
-                HStack {
-                    Text("Hallstatt")
-                        .font(.subheadline)
-                    Spacer()
-                    Text("Austria")
-                        .font(.subheadline)
-                }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Hallstatter sea")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-            
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
