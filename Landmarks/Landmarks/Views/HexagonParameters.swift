914b3278cd729e381315efe0ed59ebb5da07ce30
k2645 <k2645@naver.com>
2023-07-03 12:49:53 +0900

    [feat] hexagon parameter 이용하여 badge의 background file 생성

--- Landmarks/Landmarks/Views/HexagonParameters.swift
+++ Landmarks/Landmarks/Views/HexagonParameters.swift
@@ -21,17 +21,17 @@ struct HexagonParameters {
         Segment(
             line: CGPoint(x: 0.60, y: 0.05),
             curve: CGPoint(x: 0.40, y: 0.05),
-            control: CGPoint(x: 0.54, y: 0.00)
+            control: CGPoint(x: 0.50, y: 0.00)
         ),
         Segment(
-            line: CGPoint(x: 0.05, y: 0.20),
-            curve: CGPoint(x: 0.00, y: 0.30),
-            control: CGPoint(x: 0.00, y: 0.25)
+            line: CGPoint(x: 0.05, y: 0.20 + adjustment),
+            curve: CGPoint(x: 0.00, y: 0.30 + adjustment),
+            control: CGPoint(x: 0.00, y: 0.25 + adjustment)
         ),
         Segment(
-            line: CGPoint(x: 0.00, y: 0.70),
-            curve: CGPoint(x: 0.05, y: 0.80),
-            control: CGPoint(x: 0.00, y: 0.75)
+            line: CGPoint(x: 0.00, y: 0.70 - adjustment),
+            curve: CGPoint(x: 0.05, y: 0.80 - adjustment),
+            control: CGPoint(x: 0.00, y: 0.75 - adjustment)
         ),
         Segment(
             line: CGPoint(x: 0.40, y: 0.95),
@@ -39,13 +39,13 @@ struct HexagonParameters {
             control: CGPoint(x: 0.50, y: 1.00)
         ),
         Segment(
-            line: CGPoint(x: 0.95, y: 0.80),
-            curve: CGPoint(x: 1.00, y: 0.70),
-            control: CGPoint(x: 1.00, y: 0.75)
+            line: CGPoint(x: 0.95, y: 0.80 - adjustment),
+            curve: CGPoint(x: 1.00, y: 0.70 - adjustment),
+            control: CGPoint(x: 1.00, y: 0.75 - adjustment)
         ),Segment(
-            line: CGPoint(x: 1.00, y: 0.30),
-            curve: CGPoint(x: 0.95, y: 0.20),
-            control: CGPoint(x: 1.00, y: 0.25)
+            line: CGPoint(x: 1.00, y: 0.30 + adjustment),
+            curve: CGPoint(x: 0.95, y: 0.20 + adjustment),
+            control: CGPoint(x: 1.00, y: 0.25 + adjustment)
         )
     ]
 }
