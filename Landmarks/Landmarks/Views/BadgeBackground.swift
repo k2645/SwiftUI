ce2adbbb0e793b06fa2cf2429018d4af2a7529c2
k2645 <k2645@naver.com>
2023-07-03 12:27:11 +0900

    [add] hexagon 뱃지를 만들기위한 기본 parameter 파일 생성

---
+++ Landmarks/Landmarks/Views/BadgeBackground.swift
@@ -0,0 +1,23 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by 김영현 on 2023/07/03.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            
+        }
+        .fill(.black)
+    }
+}
+
+struct BadgeBackground_Previews: PreviewProvider {
+    static var previews: some View {
+        BadgeBackground()
+    }
+}
